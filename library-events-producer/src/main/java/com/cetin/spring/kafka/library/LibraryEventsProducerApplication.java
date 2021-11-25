ca4db6671c67e870c93bc8b11564f60cb26ec9b2
mehmetali.cetin <mehmetali.cetin@i2i-systems.com>
2021-11-25 11:11:57 +0300

    initial commit

---
+++ library-events-producer/src/main/java/com/cetin/spring/kafka/library/LibraryEventsProducerApplication.java
@@ -0,0 +1,13 @@
+package com.cetin.spring.kafka.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryEventsProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryEventsProducerApplication.class, args);
+	}
+
+}
---
+++ library-events-producer/src/main/java/com/cetin/spring/kafka/library/domain/Book.java
@@ -0,0 +1,17 @@
+package com.cetin.spring.kafka.library.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Book {
+    private Integer bookId;
+    private String  bookName;
+    private String  bookAuthor;
+}
